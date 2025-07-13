f252f983d2a76840f254d881048c1af6c7235de8
rajusunagar <rajusunagarcs@gmail.com>
2025-07-14 02:01:24 +0530

    added

--- src/main/java/dsa/array/TwoSum.java
+++ src/main/java/dsa/array/TwoSum.java
@@ -28,7 +28,7 @@ public class TwoSum {
     
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
-        int target = 10;
+        int target = 9;
         TwoSum twoSum = new TwoSum();
         int[] result = twoSum.twoSum(nums, target);
         System.out.println(result[0] + " " + result[1]);
