b309613f06bc8d4c5452a4a9f00c7c0231a206aa
Raju <rajuyamanappa.sunagar@delta.com>
2025-07-16 21:14:40 +0530

    added

--- src/main/java/dsa/array/TwoSum.java
+++ src/main/java/dsa/array/TwoSum.java
@@ -9,7 +9,7 @@ Example 1:
 
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
